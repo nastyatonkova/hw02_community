43fa55b20bcd6f391f33c737f0f23da2381d4728
Anastasiia Tonkova <nast-ton@mail.ru>
2022-04-20 16:32:58 +0200

    added Email-Form for reset passwords

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -132,3 +132,8 @@ STATIC_URL = '/static/'
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 LOGOUT_REDIRECT_URL = 'posts:index'
+
+# Email Simulation
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
